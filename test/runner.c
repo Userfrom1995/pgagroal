b843bdbe2446c037144933be2bc2d0bb9e2c0ec4
User1995 <userfrom1995@gmail.com>
2025-08-26 13:34:31 +0000

    docker issue

--- test/runner.c
+++ test/runner.c
@@ -31,13 +31,16 @@
 #include "testcases/pgagroal_test_1.h"
 #include "testcases/pgagroal_test_2.h"
 
+char* user = NULL;
+char* database = NULL;
+
 int
 main(int argc, char* argv[])
 {
 
-   if (argc != 2)
+   if (argc != 4)
    {
-      printf("Usage: %s <project_directory>\n", argv[0]);
+      printf("Usage: %s <project_directory> <user> <database>\n", argv[0]);
       return 1;
    }
 
@@ -45,6 +48,9 @@ main(int argc, char* argv[])
    Suite* s1;
    Suite* s2;
    SRunner* sr;
+
+   user = strdup(argv[2]);
+   database = strdup(argv[3]);
 
    if (pgagroal_tsclient_init(argv[1]))
    {
@@ -61,11 +67,15 @@ main(int argc, char* argv[])
    srunner_run_all(sr, CK_VERBOSE);
    number_failed = srunner_ntests_failed(sr);
    srunner_free(sr);
+   free(user);
+   free(database);
 
    pgagroal_tsclient_destroy();
    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 
 error:
    pgagroal_tsclient_destroy();
+   free(user);
+   free(database);
    return EXIT_FAILURE;
 }
